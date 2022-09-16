56a3307d7f7a1fdb4199865d480af3175ec5f3de
Serhan <srhncetin4@gmail.com>
2022-09-16 18:39:57 +0300

    array states

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   const [name, setName] = useState("Mehmet");
   const [age, setAge] = useState(25);
+  const [friends, setFriends] = useState(['Ahmet',"Mehmet","Ali"]);
 
   return (
     <div>
@@ -12,6 +13,18 @@ function App() {
       <h2>Yaşınız: {age}</h2>
       <button onClick={()=> setName("Ahmet")}>Change Name</button>
       <button onClick={()=> setAge(30)}>Change Age</button>
+
+      <hr />
+      <br></br>
+
+      <h2>Friends</h2>
+      {
+        friends.map((friend,key) =>(
+          <div key={key}>{friend}</div>
+        ))
+
+      }
+      <button onClick={()=> setFriends([...friends,"Haydar"])}>Add New Friend</button>
     </div>
   );
 }
